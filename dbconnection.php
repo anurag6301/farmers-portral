e50baef68c74abc273392844e8976e30e42ef13b
Amit <amitshar821@gmail.com>
2021-12-13 00:54:12 +0530

    Added index file and composer.json

---
+++ dbconnection.php
@@ -0,0 +1,6 @@
+<?php
+error_reporting(E_ALL & ~E_NOTICE  &  ~E_STRICT  &  ~E_WARNING);
+// Create connection
+$con=mysqli_connect("sql6.freemysqlhosting.net","sql6458366","XHEAlkXJvE","sql6458366");
+echo mysqli_connect_error();
+?>
